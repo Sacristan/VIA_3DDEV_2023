074f101f050e2d10d1dbcec76e3aead705208eb4
Ģirts Ķesteris <girts.kesteris@gmail.com>
2023-03-31 17:25:43 +0300

    initial commit

---
+++ Assets/TestCode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode : MonoBehaviour
+{
+    void Start()
+    {
+        // throw new System.Exception();
+    }
+}
