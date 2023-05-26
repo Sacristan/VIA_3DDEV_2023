ea719fb3507dc3d9ae97d165fd90f4f0bc862f1d
Ģirts Ķesteris <girts.kesteris@gmail.com>
2023-05-26 17:16:38 +0300

    add tutorial

--- Assets/_root/Scripts/UIManager.cs
+++ Assets/_root/Scripts/UIManager.cs
@@ -6,11 +6,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    static readonly string[] TutorialTexts = new string[]
+    {
+        "<color=green>COLLECT</color> ALL ESCAPED CHICKENS!",
+        "AVOID THEM <color=red>EGGS</color> - THEY DANGEROUS!",
+        "<color=green>WASD</color> TO MOVE!\n <color=green>MOUSE</color> TO LOOK AROUND!"
+    };
+    const string TutorialPrefsKey = "TimesTutorialSeen";
     [SerializeField] TMPro.TextMeshProUGUI objectiveText;
     [SerializeField] RectTransform victoryPanel;
     [SerializeField] RectTransform gameoverPanel;
 
     [SerializeField] Slider healthSlider;
+    [SerializeField] TMPro.TextMeshProUGUI tutorialText;
 
     private void Start()
     {
@@ -19,6 +27,7 @@ public class UIManager : MonoBehaviour
 
         GameManager.instance.OnObjectiveCountChanged += OnObjectiveCountChanged;
         GameManager.instance.Player.OnHealthUpdated += OnPlayerHealthUpdated;
+        HandleTutorial();
     }
 
     private void OnGameWon()
@@ -39,5 +48,54 @@ public class UIManager : MonoBehaviour
     void OnPlayerHealthUpdated(int health)
     {
         healthSlider.value = Mathf.InverseLerp(0, Player.MaxHealth, health);
+    }
+
+    public int TimesTutorialSeen
+    {
+        get
+        {
+            int timesTutorialSeen = 0;
+
+            if (PlayerPrefs.HasKey(TutorialPrefsKey))
+            {
+                timesTutorialSeen = PlayerPrefs.GetInt(TutorialPrefsKey);
+            }
+
+            return timesTutorialSeen;
+        }
+    }
+
+    void HandleTutorial()
+    {
+        if (TimesTutorialSeen < 3)
+        {
+            StartCoroutine(TutorialRoutine());
+        }
+    }
+
+    IEnumerator TutorialRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < TutorialTexts.Length; i++)
+        {
+            ShowTutorialText(i);
+            yield return new WaitForSeconds(3f);
+        }
+
+        HideTutorialText();
+
+        PlayerPrefs.SetInt(TutorialPrefsKey, TimesTutorialSeen + 1);
+
+        void ShowTutorialText(int index)
+        {
+            string txt = TutorialTexts[index];
+            tutorialText.text = txt;
+        }
+
+        void HideTutorialText()
+        {
+            tutorialText.text = string.Empty;
+        }
     }
 }
